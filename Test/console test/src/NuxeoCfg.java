b224df759cda38436616f0c6caa5713f17197e36
netlocity <netlocity@users.noreply.github.com>
2016-06-22 13:25:56 -0600

    Delete NuxeoCfg.java

--- Test/console test/src/NuxeoCfg.java
+++
@@ -1,7 +0,0 @@
-
-public class NuxeoCfg {
-	public static String URL_HTTP_AUTOMATION_CLIENT = "http://11.11.11.63:8080/nuxeo/site/automation";
-	public static String NUXEO_USERNAME = "Administrator";
-	public static String NUXEO_PASSWORD = "Welcome1";
-	public static String NUXEO_DIR_PATH = "/Default domain/Workspaces/Testing";
-}
