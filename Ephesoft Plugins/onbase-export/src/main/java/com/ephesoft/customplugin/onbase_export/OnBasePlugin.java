6c563b3237ab4fc54bee241e74540445368da2e5
mark carter <mark.carter@netlocity.com>
2016-06-22 11:01:17 -0600

    initial checkin of cspro

--- Ephesoft Plugins/onbase-export/src/main/java/com/ephesoft/customplugin/onbase_export/OnBasePlugin.java
+++
@@ -1,7 +0,0 @@
-package com.ephesoft.customplugin.onbase_export;
-import com.ephesoft.dcma.core.DCMAException;
-import com.ephesoft.dcma.da.id.BatchInstanceID;
-
-public interface OnBasePlugin {
-	void execute(final BatchInstanceID batchInstanceID, final String pluginWorkflow) throws DCMAException;
-}
